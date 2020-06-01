d77fa4d249ca65e9339712aaa776d9220ce47f74
Hitman <hackzs@outlook.com>
2020-06-01 23:25:13 +0530

    Update the Dynamica port

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const multer = require('multer');
 let upload = multer();
 let app = express();
+const port = 2000;
 
 // app.use(cookieParser());
 app.use(bodyParser.json());
@@ -13,6 +14,6 @@ app.use(upload.array());
 let movies = require('./movies.js');
 app.use('/movies', movies);
 
-app.listen(2000, function() {
-    console.log('Server starts at 2000');
+app.listen(process.env.PORT || port, function() {
+    console.log(`server starts at http://localhost:${port}`);
 });
